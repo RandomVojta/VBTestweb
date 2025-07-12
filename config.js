4e4bfae840dfdeae7663132a1b83b14946234c57
RandomVojta <155780567+RandomVojta@users.noreply.github.com>
2025-07-12 14:58:39 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,27 @@
+// config.js - sem upravuj obsah podle sebe
+
+const serverConfig = {
+  ip: "cs16.randomguy.cz",
+  port: "27015",
+  rules: [
+    "Žádný cheating nebo cheat nástroje.",
+    "Respektujte ostatní hráče.",
+    "Zákaz teamkillingu.",
+    "Používejte vhodný nick a profilovou fotku.",
+    "Dodržujte pravidla serveru i komunity."
+  ],
+  contactEmail: "admin@randomguy.cz",
+  news: [
+    {
+      date: "2025-07-10",
+      title: "Nová mapa de_mirage přidána!",
+      content: "Server nyní podporuje populární mapu de_mirage."
+    },
+    {
+      date: "2025-06-20",
+      title: "Upgrade serverového hardwaru",
+      content: "Zvýšili jsme výkon serveru pro lepší stabilitu."
+    }
+  ],
+  gametrackerImageUrl: "https://cache.gametracker.com/server_info/cs16.randomguy.cz:27015/b_350_20_692108_381007_ffffff_333333.png"
+};
